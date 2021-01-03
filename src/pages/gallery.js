2d57d6ccfccb18b57d2282d3e7c9d836649fe6c0
NullDefault <davisha999@gmail.com>
2021-01-02 22:26:36 -0500

    small gallery styling

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,4 +1,4 @@
-import {Box, Center, useColorMode,  SimpleGrid} from "@chakra-ui/react";
+import {Box, Center, useColorMode, SimpleGrid} from "@chakra-ui/react";
 import {BackgroundContainer} from "../components/BackgroundContainer";
 import {Footer} from "../components/Footer/Footer";
 import {TopNavigation} from "../components/TopNavigation/TopNavigation";
@@ -9,11 +9,20 @@ export default function Gallery() {
     const {colorMode} = useColorMode();
     const bgColor = {light: 'green.400', dark: 'green.900'};
     const color = {light: 'black', dark: 'white'};
+    const cardBgColor = {light: 'gray.100', dark: 'gray.500'}
 
     const windowSize = useWindowSize();
 
     const cardHeight = windowSize.height / 3;
     const cardWidth = windowSize.width / 3;
+
+    const projects = [
+        {name: 'Tira', species: 42},
+        {name: 'Darwin IV', species: 26},
+        {name: 'Serena', species: 42},
+        {name: 'Future Earth', species: 31},
+        {name: 'Terrania', species: 12}
+    ];
 
     return (
         <Box>
@@ -23,14 +32,17 @@ export default function Gallery() {
                     <SideDrawer/>
                 </Box>
 
-                <Box bgColor={bgColor[colorMode]} color={color[colorMode]} w={windowSize.width * .90} alignContent="center" my='32px' mx='auto'>
+                <Box bgColor={bgColor[colorMode]} color={color[colorMode]} w={windowSize.width * .90}
+                     alignContent="center" my='32px' mx='auto' style={{borderRadius: "24px"}}>
                     <SimpleGrid columns={2} spacing={10} p="24px">
-                        <Box mx="auto" bg="white" height={cardHeight} width={cardWidth}/>
-                        <Box mx="auto" bg="white" height={cardHeight} width={cardWidth}/>
-                        <Box mx="auto" bg="white" height={cardHeight} width={cardWidth}/>
-                        <Box mx="auto" bg="white" height={cardHeight} width={cardWidth}/>
-                        <Box mx="auto" bg="white" height={cardHeight} width={cardWidth}/>
-                        <Box mx="auto" bg="white" height={cardHeight} width={cardWidth}/>
+                        {projects.map((item, i) =>
+                            <Box key={i} w={cardWidth} h={cardHeight} bgColor={cardBgColor[colorMode]}
+                                 style={{borderRadius: "24px"}}>
+                                <Center>
+                                    name: {item.name} species: {item.species}
+                                </Center>
+                            </Box>)
+                        }
                     </SimpleGrid>
                 </Box>
             </BackgroundContainer>
